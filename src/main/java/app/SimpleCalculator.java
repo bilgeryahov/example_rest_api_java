c0113a9fb5864450857af7c19fd276411bc7b21e
Bilger Yahov <bilger@studyportals.com>
2017-12-01 13:06:47 +0100

    Add SimpleCalculator class and a test class for it.

---
+++ src/main/java/app/SimpleCalculator.java
@@ -0,0 +1,11 @@
+package app;
+
+public class SimpleCalculator {
+    public static int sum(int a, int b){
+        return a+b;
+    }
+
+    public static int subtract(int a, int b){
+        return a-b;
+    }
+}
---
+++ src/test/java/app/SimpleCalculatorTest.java
@@ -0,0 +1,21 @@
+package app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SimpleCalculatorTest {
+    @Test
+    public void sumTry(){
+        int x  = 1;
+        int y = 1;
+        assertEquals(2, SimpleCalculator.sum(x,y));
+    }
+
+    @Test
+    public void subtractTry(){
+        int x  = 2;
+        int y = 1;
+        assertEquals(1, SimpleCalculator.subtract(x,y));
+    }
+}
