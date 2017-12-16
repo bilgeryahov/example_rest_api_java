172d1ded8c1edc7c8b438f5eb7b07bb2f1ce46fd
Bilger Yahov <bilger@studyportals.com>
2017-12-16 14:45:02 +0100

    Open Tests.

---
+++ src/test/java/SpiderTest.java
@@ -0,0 +1,50 @@
+import org.junit.*;
+
+import static org.hamcrest.Matchers.samePropertyValuesAs;
+import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.equalTo;
+
+
+public class SpiderTest {
+
+    @Test
+    public void testCrawlingForEntireWebsite(){
+
+    }
+
+    @Test
+    public void testCrawlingForSpecificMovie(){
+
+    }
+
+    @Test
+    public void testCrawlingForSpecificMusic(){
+
+    }
+
+    @Test
+    public void testCrawlingForSpecificBook(){
+    }
+
+    @Test
+    public void testDetailsAboutEntireWebsiteCrawl(){
+
+    }
+
+    @Test
+    public void testDetailsAboutSpecificMovieCrawl(){
+
+    }
+
+    @Test
+    public void testDetailsAboutSpecificMusicCrawl(){
+
+    }
+
+    @Test
+    public void testDetailsAboutSpecificBookCrawl(){
+
+    }
+}
